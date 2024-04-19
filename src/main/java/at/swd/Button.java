4471a353a257adb7b99c9304fe843429ee1c4250
Julian F. <j.fiedler2002@outlook.com>
2024-04-19 21:11:06 +0200

    Created necessary interfaces

---
+++ src/main/java/at/swd/Button.java
@@ -0,0 +1,5 @@
+package at.swd;
+
+public interface Button {
+    void render();
+}
---
+++ src/main/java/at/swd/GUIFactory.java
@@ -0,0 +1,6 @@
+package at.swd;
+
+public interface GUIFactory {
+    public Button createButton();
+    public Checkbox createCheckbox();
+}
