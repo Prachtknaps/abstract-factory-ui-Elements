5ee013675244f546dcc8c70b196f0e80b0c57d53
Julian F. <j.fiedler2002@outlook.com>
2024-04-19 21:20:55 +0200

    Implemented final abstract factory logic in Main.java

--- src/main/java/at/swd/Main.java
+++ src/main/java/at/swd/Main.java
@@ -2,6 +2,19 @@ package at.swd;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GUIFactory factory;
+        String osName = System.getProperty("os.name");
+
+        if (osName.toLowerCase().contains("windows")) {
+            factory = new WindowsFactory();
+        } else {
+            factory = new MacOSFactory();
+        }
+
+        Button button = factory.createButton();
+        Checkbox checkbox = factory.createCheckbox();
+
+        button.render();
+        checkbox.render();
     }
 }
